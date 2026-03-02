ebe5602e9c2441b109cedf1f6c2101e83cff4a95
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-02 18:01:52 +0100

    Create ClockConfig.java

---
+++ src/main/java/com/shotaroi/integrationhub/common/time/ClockConfig.java
@@ -0,0 +1,15 @@
+package com.shotaroi.integrationhub.common.time;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
+}
